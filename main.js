e1f602f7d7d598f8aadc7995010d937c793f10c8
Cédric David <cedric.david@btopenworld.com>
2018-11-22 10:37:11 +1000

    Create main.js

---
+++ main.js
@@ -0,0 +1,29 @@
+$(document).ready(() => {
+  $('.hint-box').on('click', () => {
+    $('.hint').slideToggle(500);
+  });
+  
+  $('.wrong-answer-one').on('click', () => {
+    $('.wrong-text-one').fadeOut('slow');
+    $('.frown').show();
+  });
+  
+  $('.wrong-answer-two').on('click', () => {
+    $('.wrong-text-two').fadeOut('fast');
+    $('.frown').show();
+  });
+  
+  $('.wrong-answer-three').on('click', () => {
+    $('.wrong-text-three').fadeOut('slow');
+    $('.frown').show();
+  });
+  
+  $('.correct-answer').on('click', () => {
+    $('.frown').hide();
+    $('.smiley').show();
+    $('.wrong-text-one').fadeOut();
+    $('.wrong-text-two').fadeOut();
+    $('.wrong-text-three').fadeOut();
+  });
+
+});
